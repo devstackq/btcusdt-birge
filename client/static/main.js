9feefc14af4e10006b9da919d6e5cbfe6dea800b
devstackq <loginpsn@mail.ru>
2021-08-03 00:43:33 +0600

    step 3 try render data inside svg, like graph

--- client/static/main.js
+++ client/static/main.js
@@ -6,20 +6,85 @@ document.addEventListener("DOMContentLoaded", () => {
     socket.send(JSON.stringify({ type: "getWsBinanceData" }));
   };
 
+  // let circles = [];
+  // append current svg, new line, circle
+  // time, last 4 elems, y - dollar
+
+  function render(points) {
+    // console.log(points.length);
+    let circles = [];
+    let polyline = document.getElementById("line");
+    svg = document.getElementById("chartSvg");
+    svg.innerHtml = "";
+    if (polyline == null) {
+      polyline = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "polyline"
+      );
+      polyline.setAttributeNS(null, "id", "line");
+      svg.appendChild(polyline);
+    }
+
+    let polylinePoints = [];
+    for (let i = 0; i < points.length; i++) {
+      polylinePoints.push(points[i].x + ", " + points[i].y);
+      let circle;
+      // console.log(circles.length);
+      if (i < circles.length) {
+        circle = circles[i];
+      } else {
+        circle = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "circle"
+        );
+        circle.setAttributeNS(
+          null,
+          "r",
+          40
+        ); /* This is the radius of the circle */
+        circle.setAttributeNS(
+          null,
+          "class",
+          "point"
+        ); /* You can style individual shapes using CSS */
+        circles.push(circle);
+        svg.appendChild(circle);
+      }
+      circle.setAttributeNS(null, "cx", points[i].x);
+      circle.setAttributeNS(null, "cy", points[i].y);
+    }
+    /* In case we modify the number of points */
+    if (points.length < circles.length) {
+      for (; i < circles.length; i++) {
+        circles[i].remove();
+      }
+      circles.splice(points.length, circles.length);
+    }
+    polyline.setAttributeNS(null, "points", polylinePoints.join(" "));
+  }
+
   socket.onmessage = function (e) {
     let app = document.getElementById("app");
-    let chart = document.getElementById("chart");
+    let chart = document.getElementById("chartSvg");
     let message = JSON.parse(e.data);
-    console.log("Получены данные " + Object.entries(message), e.data);
-
+    //array data -? paint graphic ||
+    //prev innerHtml = "", create new dynamic js
     switch (message[0].type) {
       case "newdata":
-        for (let [k, v] of Object.entries(message)) {
+        let points = [];
+        for (let [idx, v] of Object.entries(message)) {
           // app.textContent = ` ${v.bids}  : ${v.asks}, ${v.spread}`;
+          // points.push({ x: v.time, y: v.maxask });
+          // let t = v.time.toString().slice(5)
+          console.log(10 + idx);
+          points = [...points, { x: 100 + idx, y: v.maxask }];
+          render(points);
         }
+        //1, {110, 41.000}
+        //2, {111, 41.200}
         socket.send(JSON.stringify({ type: "getWsBinanceData" }));
       default:
-        console.log("default");
+        console.log("default case");
     }
   };
 
--- main.go
+++ main.go
@@ -5,31 +5,30 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-// type JsonResponse struct {
-// 	Bids []Bid `json:"bids"`
-// 	Asks []Bid `json:"asks"`
-// }
-
-// type Bid struct {
-// 	PriceLevel string
-// 	Quantity   string
-// }
 //bids - спрос(покупатель заявляет цену за 1 ед и объем - актив который хочет купить) 	Desc, 40.9, 40.5, 40.3
 //asks - предложение(продавец завяляет цену за 1 ед и объем - актив которы хочет продать) asc,  40.1, 40.2, 40,5
 
 // how to render graphic ? get prev data , andalyze, then show new graph ?
 // {"lastUpdateId":12690389951,"bids":[["41634.03000000","0.46010100"],["41632.20000000","0.03058500"],["41632.18000000","0.08230900"],["41632.00000000","0.00240200"],["41631.01000000","0.04946700"]],"asks":[["41634.04000000","0.80552300"],["41634.73000000","0.17000000"],["41636.31000000","0.59072500"],["41636.47000000","0.26180500"],["41636.48000000","0.14400000"]]}
 type JsonResponse struct {
-	// Mu       sync.Mutex
-	Sequence int64           `json:"lastupdateid"`
-	Bids     [][]interface{} `json:"bids"`
-	Asks     [][]interface{} `json:"asks"`
-	Spread   float64         `json:"spread"`
-	Type     string          `json:"type"`
+	Mu     sync.Mutex
+	LastId int64   `json:"lastupdateid"`
+	Spread float64 `json:"spread"`
+	Type   string  `json:"type"`
+	Time   int64   `json:"time"`
+	MinBid float64 `json:"minbid"`
+	MaxAsk float64 `json:"maxask"`
+}
+
+type WsBinaceData struct {
+	Bids [][]interface{} `json:"bids"`
+	Asks [][]interface{} `json:"asks"`
 }
 
 type WsType struct {
@@ -60,8 +59,7 @@ func handleWsClient(w http.ResponseWriter, r *http.Request) {
 	}
 	go wsGetBtcUdts()
 
-	wsType := WsType{}
-
+	var wsType WsType
 	var seqDepth = []JsonResponse{}
 
 	for {
@@ -81,13 +79,10 @@ func handleWsClient(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if wsType.Name == "getWsBinanceData" {
-			seqDepth = append(seqDepth, <-data)
-			// b, err := json.Marshal(seqDepth)
-			// if err != nil {
-			// 	panic(err)
-			// }
-			// seqDepth.Type = "data"
-			conn.WriteJSON(seqDepth)
+			go func() {
+				seqDepth = append(seqDepth, <-data)
+				conn.WriteJSON(seqDepth)
+			}()
 		}
 		defer conn.Close()
 	}
@@ -95,38 +90,37 @@ func handleWsClient(w http.ResponseWriter, r *http.Request) {
 
 //ticker - 1 sec, get new data from trade - aggTrade -> put data in ResponseData
 func wsGetBtcUdts() {
-	//client ws
 	//@depth20@100ms get count 20 bids and 20 asks - bidth each 100ms
 	c, _, err := websocket.DefaultDialer.Dial("wss://stream.binance.com:9443/ws/btcusdt@depth5@1000ms", nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
-	done := make(chan struct{})
 
-	// go func() {
-	defer close(done)
+	jsonData := JsonResponse{}
+	binData := WsBinaceData{}
+
 	for {
-		jsonData := JsonResponse{}
-		err = c.ReadJSON(&jsonData) //bid, ask
+		err = c.ReadJSON(&binData) //bid, ask
 		if err != nil {
 			log.Println("read:", err)
 			return
 		}
-		//jsonData.Mu.Lock()
-		//bid := make(chan float64)
 		// go func() {
-		minBid := jsonData.Bids[len(jsonData.Asks)-1][0].(string)
+		minBid := binData.Bids[len(binData.Asks)-1][0].(string)
 		mb, _ := strconv.ParseFloat(minBid, 64)
-		maxAsk := jsonData.Asks[len(jsonData.Asks)-1][0].(string)
+		jsonData.MinBid = mb
+
+		maxAsk := binData.Asks[len(binData.Asks)-1][0].(string)
 		ma, _ := strconv.ParseFloat(maxAsk, 64)
+		jsonData.MaxAsk = ma
+
 		jsonData.Spread = ma - mb
-		// }()
 		jsonData.Type = "newdata"
-		// log.Println(jsonData, jsonData.Spread, "data")
+		jsonData.Time = time.Now().Local().Unix()
+		//x, y = todo here ?
 		data <- jsonData
 	}
-	// }()
 }
 
 func main() {
